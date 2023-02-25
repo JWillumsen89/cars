525dcacdcead161b37255ae5a30ee21c6255899a
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-02-25 16:10:19 +0100

    Added small changes to navbar.js

--- src/main/resources/static/javascript/navbar.js
+++ src/main/resources/static/javascript/navbar.js
@@ -1,11 +1,12 @@
 //Adds buttons to the navbar
-let navbar = '<nav>'
-navbar += '<button id="home-btn">Home</button>';
-navbar += '<button id="cars-btn">Cars</button>';
-navbar += '<button id="members-btn">Members</button>';
-navbar += '</nav>';
-const headerContent = document.getElementById('navbar-content');
-headerContent.innerHTML = navbar;
+const navbarContent = document.getElementById('navbar-content');
+navbarContent.innerHTML =  `
+ <nav>
+<button id="home-btn">Home</button>
+<button id="cars-btn">Cars</button>
+<button id="members-btn">Members</button>
+</nav>
+`;
 
 // Add event listeners to the navbar buttons
 document.getElementById('home-btn').addEventListener('click', showHomePage);
